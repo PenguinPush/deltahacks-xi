e9fa829898057e8ddabe5182d3740ad4681d42f3
PenguinPush <icyzminecraft@gmail.com>
2025-01-11 13:00:39 -0500

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -1,19 +1,27 @@
-from flask import Flask, request, redirect
+from flask import Flask, send_from_directory, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend')
 
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
     resp = MessagingResponse()
 
     # Add a message
     resp.message("The Robots are coming! Head for the hills!")
 
-    return resp
+    return str(resp)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 
 if __name__ == "__main__":
